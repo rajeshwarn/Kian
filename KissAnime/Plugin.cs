ee38c5f030a871a4b02d344cbb7d4097aa0dfaaf
wildbook <book.wille@gmail.com>
2016-04-27 14:59:04 +0200

    MAL plugin is now completely functional.

---
+++ KissAnime/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using Kian.Core;
+using System.ComponentModel.Composition;
+using System.Windows.Controls;
+
+namespace KissAnime
+{
+    [Export(typeof(IPlugin))]
+    public class Plugin : IPlugin
+    {
+        private string _name = "KissAnime";
+        private ContentControl _pluginContent;
+        private WPF wpf;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public ContentControl PluginContent
+        {
+            get
+            {
+                return _pluginContent;
+            }
+
+            set
+            {
+                _pluginContent = value;
+            }
+        }
+
+        public void OnSearch(string searchString)
+        {
+            if (wpf.Authenticated)
+                wpf.OnSearch(searchString);
+        }
+
+        public void OnStart()
+        {
+            PluginContent.Content = wpf = new WPF();
+        }
+    }
+}
--- KissAnime/WPF.xaml.cs
+++ KissAnime/WPF.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using Kian.Core;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
-using MyAnimeList.API.Anime;
-using MyAnimeList.Objects.API.Anime;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,7 +11,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace MyAnimeList
+namespace KissAnime
 {
     /// <summary>
     /// Interaction logic for WPF.xaml
@@ -33,11 +31,9 @@ namespace MyAnimeList
             InitializeComponent();
         }
 
-        private NetworkCredential _login;
-        private bool _authenticated = false;
-        private bool _saveLogin = false;
-        private List<animeEntry> _searchItems = new List<animeEntry>();
+        //private List<animeEntry> _searchItems = new List<animeEntry>();
         private Visibility _display = Visibility.Collapsed;
+
         private string _searchItemsCountString = "Ready!";
         private bool _searching = false;
         private bool _enabled = false;
@@ -91,26 +87,13 @@ namespace MyAnimeList
                 NotifyPropertyChanged("Authenticated");
 
                 if (value == true)
-                {
                     Display = Visibility.Visible;
-                    if (_saveLogin == true)
-                    {
-                        Properties.Settings.Default.Username = _login.UserName;
-                        Properties.Settings.Default.Password = Crypt.EncryptString(_login.SecurePassword);
-                        Properties.Settings.Default.Save();
-                    }
-                }
                 else
-                {
                     Display = Visibility.Collapsed;
-
-                    Properties.Settings.Default.Username = null;
-                    Properties.Settings.Default.Password = null;
-                    Properties.Settings.Default.Save();
-                }
             }
         }
 
+        /*
         public List<animeEntry> SearchItems
         {
             get
@@ -123,6 +106,7 @@ namespace MyAnimeList
                 NotifyPropertyChanged("SearchItems");
             }
         }
+        */
 
         public Visibility Display
         {
@@ -154,6 +138,7 @@ namespace MyAnimeList
         {
             BackgroundWorker bw = new BackgroundWorker();
 
+            /*
             bw.DoWork += delegate
             {
                 Searching = true;
@@ -177,12 +162,14 @@ namespace MyAnimeList
                 else
                     SearchItemsCountString = SearchItems.Count + " Results";
             };
+            */
 
             bw.RunWorkerAsync();
         }
 
         private void Anime_Expanded(object sender, RoutedEventArgs e)
         {
+            /*
             Expander expander = (Expander)sender;
 
             animeEntry entry = (animeEntry)expander.DataContext;
@@ -200,83 +187,11 @@ namespace MyAnimeList
                     entry = null;
                 }
             }
+            */
         }
 
-        private async void UserControl_Loaded(object sender, EventArgs e)
+        private void UserControl_Loaded(object sender, EventArgs e)
         {
-            _login = new NetworkCredential(Properties.Settings.Default.Username, Crypt.DecryptString(Properties.Settings.Default.Password));
-            if (API.Authentication.VerifyCredentials(_login))
-                Authenticated = true;
-            else
-            {
-                Authenticated = false;
-                await Authenticate();
-            }
-        }
-
-        private async Task<bool> Authenticate()
-        {
-            MetroDialogSettings approveLoginDialogSettings = new MetroDialogSettings
-            {
-                AffirmativeButtonText = "Yes",
-                NegativeButtonText = "No",
-                FirstAuxiliaryButtonText = "Yes, but don't save my password",
-                DefaultButtonFocus = MessageDialogResult.Affirmative,
-                AnimateHide = true,
-                AnimateShow = true,
-            };
-
-            MetroWindow mainWindow = ((MetroWindow)Window.GetWindow(this));
-            var approveLogin = await mainWindow.ShowMessageAsync("Do you want to log in with your MyAnimeList account?", "If you don't log in, Kian will not be able to read info about shows from MAL", MessageDialogStyle.AffirmativeAndNegativeAndSingleAuxiliary, approveLoginDialogSettings);
-
-            if (approveLogin == MessageDialogResult.Negative)
-            {
-                Authenticated = false;
-                return false;
-            }
-            else if (approveLogin == MessageDialogResult.Affirmative)
-                _saveLogin = true;
-            else if (approveLogin == MessageDialogResult.FirstAuxiliary)
-                _saveLogin = false;
-
-            LoginDialogData login = await mainWindow.ShowLoginAsync("Enter your MAL login:", "By logging in, your password will be encrypted using DPAPI and saved until next time.");
-            NetworkCredential loginCredentials = new NetworkCredential(login.Username, login.Password);
-
-            if (!string.IsNullOrEmpty(loginCredentials.UserName) &&
-                !string.IsNullOrEmpty(loginCredentials.Password) &&
-                API.Authentication.VerifyCredentials(loginCredentials))
-            {
-                _login = loginCredentials;
-                Authenticated = true;
-                return true;
-            }
-            else
-            {
-                MetroDialogSettings retryDialogSettings = new MetroDialogSettings()
-                {
-                    AffirmativeButtonText = "Yes",
-                    NegativeButtonText = "No",
-                    AnimateHide = true,
-                    AnimateShow = true,
-                    DefaultButtonFocus = MessageDialogResult.Affirmative,
-                };
-
-                MessageDialogResult retry = await mainWindow.ShowMessageAsync("Your MyAnimeList login could not be verified.", "Do you want to try re-entering it?", MessageDialogStyle.AffirmativeAndNegative, retryDialogSettings);
-                if (retry == MessageDialogResult.Affirmative)
-                {
-                    return await Authenticate();
-                }
-                else
-                {
-                    Authenticated = false;
-                    return false;
-                }
-            }
-        }
-
-        private void Logout_Click(object sender, RoutedEventArgs e)
-        {
-            Authenticated = false;
         }
     }
 }
